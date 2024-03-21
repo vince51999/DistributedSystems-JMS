0c582a61525e6272380d521c03c32272c6fb5cf9
Vincenzo Barbato <vincenzo.barbato.51999@gmail.com>
2024-03-21 10:35:53 +0100

    Removed unnecessary import

--- src/it/unipr/barbato/Controller/Broker.java
+++ src/it/unipr/barbato/Controller/Broker.java
@@ -1,16 +1,9 @@
 package it.unipr.barbato.Controller;
 
-import java.util.Set;
-
 import org.apache.activemq.ActiveMQConnection;
 import org.apache.activemq.ActiveMQConnectionFactory;
 import org.apache.activemq.broker.BrokerFactory;
 import org.apache.activemq.broker.BrokerService;
-
-import it.unipr.barbato.Model.MessageHandler;
-import it.unipr.barbato.Model.ProductsListImpl;
-import it.unipr.barbato.Model.Interface.Product;
-import it.unipr.barbato.Model.Interface.ProductsList;
 
 public class Broker {
 	private static final String BROKER_URL = "tcp://localhost:61616";
