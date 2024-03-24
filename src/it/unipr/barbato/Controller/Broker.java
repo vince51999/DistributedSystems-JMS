93fa45d6ced538b0f3b95a4550bbe0b4c069000c
Vincenzo Barbato <vincenzo.barbato.51999@gmail.com>
2024-03-24 18:21:01 +0100

    Updated javadoc and code readability

--- src/it/unipr/barbato/Controller/Broker.java
+++ src/it/unipr/barbato/Controller/Broker.java
@@ -5,26 +5,41 @@ import org.apache.activemq.ActiveMQConnectionFactory;
 import org.apache.activemq.broker.BrokerFactory;
 import org.apache.activemq.broker.BrokerService;
 
+/**
+ * The {@code Broker} class represents a message broker that handles the communication between producers and consumers.
+ * It creates and starts an ActiveMQ broker, establishes a connection, and starts the communication.
+ * 
+ * @author Vincenzo Barbato 345728
+ */
 public class Broker {
+	/**
+	 * The broker URL.
+	 */
 	private static final String BROKER_URL = "tcp://localhost:61616";
+	/**
+	 * The broker properties.
+	 */
 	private static final String BROKER_PROPS = "persistent=false&useJmx=false";
-	private static ActiveMQConnection connection = null;
+
+	/**
+	 * The main method of the Broker class.
+	 * It creates and starts an ActiveMQ broker, establishes a connection, and starts the communication.
+	 *
+	 * @param args the command-line arguments
+	 * @throws Exception if an error occurs during the execution
+	 */
 	public static void main(String[] args) throws Exception {
 
-		try {
-			BrokerService broker = BrokerFactory.createBroker("broker:(" + BROKER_URL + ")?" + BROKER_PROPS);
+		BrokerService broker = BrokerFactory.createBroker("broker:(" + BROKER_URL + ")?" + BROKER_PROPS);
 
-			broker.start();
+		broker.start();
 
-			ActiveMQConnectionFactory cf = new ActiveMQConnectionFactory(BROKER_URL);
-			cf.setTrustAllPackages(true);
+		ActiveMQConnectionFactory cf = new ActiveMQConnectionFactory(BROKER_URL);
+		cf.setTrustAllPackages(true);
 
-			connection = (ActiveMQConnection) cf.createConnection();
+		ActiveMQConnection connection = (ActiveMQConnection) cf.createConnection();
 
-			connection.start();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		connection.start();
 	}
 
 }
