1c398f732db9505bb2d76fbd6c896fba38b1c6ac
Vincenzo Barbato <vincenzo.barbato.51999@gmail.com>
2024-03-24 18:24:16 +0100

    Updated client

--- src/it/unipr/barbato/Controller/Client.java
+++ src/it/unipr/barbato/Controller/Client.java
@@ -3,9 +3,19 @@ package it.unipr.barbato.Controller;
 import java.util.ArrayList;
 import java.util.Random;
 
-import it.unipr.barbato.Model.MessageHandler;
-import it.unipr.barbato.Model.ServerOnMessage;
+import it.unipr.barbato.Interface.Product.ProductsList;
+import it.unipr.barbato.Model.Message.ClientOfferHandler;
+import it.unipr.barbato.Model.Message.ClientProductsHandler;
+import it.unipr.barbato.Model.Message.MessageHandlerImpl;
+import it.unipr.barbato.Model.Message.RequestType;
 
+/**
+ * The {@code Client} class represents a client that interacts with the shop system.
+ * It subscribes to the products list, creates client offers, and sends them to the server.
+ * The client continues to make purchases until a specified number of purchases is reached.
+ * 
+ * @author Vincenzo Barbato 345728
+ */
 public class Client {
 	/**
 	 * Min price for product
@@ -19,88 +29,70 @@ public class Client {
 	 * Number of purchases that client do before logout from the shop
 	 */
 	private static final int PURCHASES = 10;
-	
-	private static final String BROKER_URL = "tcp://localhost:61616";
-	
+
 	/**
 	 * Runnable method to run client
+	 * 
 	 * @param args Arguments for main method
 	 * @throws Exception If there is a problem
 	 */
 	public static void main(String[] args) throws Exception {
 
-		//ProductOffer myOffer = new ProductOfferImpl();
-		try {
-			MessageHandler messageHandler = new MessageHandler(BROKER_URL);
-			ServerOnMessage onMessage = new ServerOnMessage();
-			messageHandler.setOnMessage(onMessage);
-			messageHandler.subscribe("productslist");
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		MessageHandlerImpl messageHandler = new MessageHandlerImpl();
+		messageHandler.start();
 
-//		ProductsOffersList offersList = (ProductsOffersList) registry.lookup("offersList");
-//		offersList.subscribe(myOffer);
-//
-//		int productsCount = 0;
-//		
-//		// Client didn't end to purchases
-//		while (productsCount < PURCHASES) {
-//			ArrayList<Integer> sns = productsList.getSNs();
-//			
-//			// If list of SN is empty client waits
-//			if (sns.size() > 0) {
-//				int sn = getRandomSN(sns);
-//				int offer = getRandomOffer();
-//				int price = productsList.getProduct(sn).getPrice();
-//				((ProductOfferImpl) myOffer).setSN(sn);
-//				((ProductOfferImpl) myOffer).setOffer(offer);
-//				System.out.println("-------------------------");
-//				System.out.println("SN: " + sn + " Price: " + price + " Offer: " + offer);
-//
-//				Boolean confirm = null;
-//				while (confirm == null) {
-//					Thread.sleep(1000);
-//					confirm = ((ProductOfferImpl) myOffer).getConfirm();
-//				}
-//				System.out.println("-------------------------");
-//				if (confirm) {
-//					productsCount++;
-//					System.out.println("Server has accepted offer! Product with sn: " + sn
-//							+ " has been bought at price: " + offer + "$");
-//				} else {
-//					System.out.println(
-//							"Server has rejected the offered price: " + offer + "$ " + " for product with sn: " + sn);
-//				}
-//
-//				myOffer.setConfirm(null);
-//				((ProductOfferImpl) myOffer).setOffer(0);
-//				((ProductOfferImpl) myOffer).setSN(0);
-//			}
-//			System.out.println(productsCount + "/" + PURCHASES);
-//			Thread.sleep(2000);
-//
-//		}
-//		System.out.println("Leave the shop.");
-//		offersList.unsubcribe(myOffer);
-//		UnicastRemoteObject.unexportObject(myOffer, false);
+		// Create client offer
+		ClientOfferHandler myOffer = new ClientOfferHandler(messageHandler);
+		myOffer.start();
+
+		// Client subscribe to products list
+		ClientProductsHandler productsList = new ClientProductsHandler(messageHandler);
+		productsList.start();
+
+		// Client don't end to purchases
+		while (myOffer.getProductsCount() < PURCHASES) {
+			ProductsList list = productsList.getProductsList();
+			if (list != null) {
+				ArrayList<Integer> sns = list.getSNs();
+				if (sns.size() > 0) {
+					int sn = getRandomSN(sns);
+					int offer = getRandomOffer();
+					int price = productsList.getProductsList().getProduct(sn).getPrice();
+					myOffer.setClientOffer(sn, offer, null);
+
+					System.out.println("-------------------------");
+					System.out.println("SN: " + sn + " Price: " + price + " Offer: " + offer);
+
+					messageHandler.send("offersList", myOffer.getClientOffer(), RequestType.offer);
+
+				}
+				System.out.println(myOffer.getProductsCount() + "/" + PURCHASES);
+				Thread.sleep(2000);
+			}
+			Thread.sleep(2000);
+		}
+		myOffer.close();
+		productsList.close();
+		messageHandler.close();
 	}
-	
+
 	/**
 	 * Return a random product SN
-	 * @param array ArrayList of products SN
+	 * 
+	 * @param sns ArrayList of products SN
 	 * @return Random product SN
 	 */
-	public static int getRandomSN(ArrayList<Integer> array) {
-		int rnd = new Random().nextInt(array.size());
-		return array.get(rnd);
+	private static int getRandomSN(ArrayList<Integer> sns) {
+		int rnd = new Random().nextInt(sns.size());
+		return sns.get(rnd);
 	}
 
 	/**
 	 * Return a random offer for a product
+	 * 
 	 * @return Random offer between MIN_PRICE and MAX_PRICE
 	 */
-	public static int getRandomOffer() {
+	private static int getRandomOffer() {
 		return new Random().nextInt(MAX_PRICE - MIN_PRICE) + MIN_PRICE;
 	}
 }
