37fcc41e0cdbc1e3e109066c003d43c665e14b2a
Vincenzo Barbato <vincenzo.barbato.51999@gmail.com>
2024-03-24 14:55:01 +0100

    Create enum with possible client requests

---
+++ src/it/unipr/barbato/Model/Message/RequestType.java
@@ -0,0 +1,13 @@
+
+package it.unipr.barbato.Model.Message;
+
+/**
+ * The {@code RequestType} enum represents the types of requests that can be made.
+ * It includes the options for subscribing and offering.
+ * 
+ * @author Vincenzo Barbato 345728
+ */
+public enum RequestType {
+	subscribe,
+	offer
+}
