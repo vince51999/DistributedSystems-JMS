32111ff3e16c3ca91961f5ab72feb3fd74d62278
Vincenzo Barbato <vincenzo.barbato.51999@gmail.com>
2024-03-24 14:51:52 +0100

    Created class ClientProductsHandler

---
+++ src/it/unipr/barbato/Model/Message/ClientProductsHandler.java
@@ -0,0 +1,66 @@
+package it.unipr.barbato.Model.Message;
+
+import java.io.Serializable;
+
+import it.unipr.barbato.Interface.Message.Handler;
+import it.unipr.barbato.Interface.Product.ProductsList;
+import jakarta.jms.JMSException;
+import jakarta.jms.Message;
+import jakarta.jms.MessageListener;
+import jakarta.jms.ObjectMessage;
+
+/**
+ * The {@code ClientProductsHandler} class implements the {@link Handler} and {@link MessageListener} interfaces.
+ * It is responsible for handling client product messages and updating the products list.
+ * 
+ *  @author Vincenzo Barbato 345728
+ */
+public class ClientProductsHandler implements Handler, MessageListener {
+	private ProductsList productsList = null;
+	private MessageHandler messageHandler = null;
+
+	/**
+	 * Constructs a ClientProductsHandler object with the specified MessageHandler.
+	 *
+	 * @param messageHandler the MessageHandler to be used for handling messages
+	 */
+	public ClientProductsHandler(MessageHandler messageHandler) {
+		this.messageHandler = messageHandler;
+	}
+
+	/**
+	 * Gets the current products list.
+	 *
+	 * @return the current products list
+	 */
+	public ProductsList getProductsList() {
+		return this.productsList;
+	}
+
+
+	@Override
+	public void start() throws JMSException {
+		messageHandler.setOnSubscribe(this);
+		messageHandler.subscribe("productsList");
+	}
+
+
+	@Override
+	public void close() throws JMSException {
+		System.out.println("Leave the shop.");
+	}
+
+
+	@Override
+	public void onMessage(Message message) {
+		try {
+			if (message instanceof ObjectMessage) {
+				ObjectMessage objectMessage = (ObjectMessage) message;
+				Serializable object = objectMessage.getObject();
+				this.productsList = (ProductsList) object;
+			}
+		} catch (JMSException e) {
+			e.printStackTrace();
+		}
+	}
+}
